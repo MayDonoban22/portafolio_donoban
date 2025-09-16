22816c3f99d3670e744d9a678a9b3151fd246716
May Donoban <rppride_2411@hotmail.com>
2025-09-16 08:49:41 -0500

    api-full url updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ function Projects() {
       titulo: "Web Project API Full",
       descripcion:
         "Full-stack application with JWT authentication and card management. Includes backend in Express/MongoDB and frontend in React.",
-      link: "web-project-api-full-swart.vercel.app",
+      link: "https://web-project-api-full-swart.vercel.app/login",
     },
     {
       titulo: "Luz de Rubí",
